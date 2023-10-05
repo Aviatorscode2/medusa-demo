9dd587f7d6c5c069851a2871c1402586e0c4e169
Aviatorscode2 <victoryakubu5@gmail.com>
2023-10-05 23:41:32 +0100

    first commit

---
+++ my-medusa-store/src/admin/routes/sales/page.tsx
@@ -0,0 +1,32 @@
+import { RouteConfig } from '@medusajs/admin';
+import { CircleStack } from '@medusajs/icons';
+// import TopCards from './components/TopCards';
+// import BarChart from './components/BarChart';
+// import RecentOrders from './components/RecentOrders';
+import { useProducts } from 'medusa-react';
+
+const Sales = () => {
+  const { products } = useProducts();
+
+  console.log(products);
+
+  return (
+    <div>
+      {/* <TopCards /> */}
+      <div className="p-4 grid grid-cols-2 gap-4">
+        {/* <BarChart /> */}
+        {/* <RecentOrders /> */}
+      </div>
+    </div>
+  );
+};
+
+// Adding route into the admin dashboard sidebar
+export const config: RouteConfig = {
+  link: {
+    label: 'Sales',
+    icon: CircleStack,
+  },
+};
+<style></style>;
+export default Sales;
